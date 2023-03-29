b6fd34037d0302169d6ba3be9c891fba7b028f78
karmsten <122186810+karmsten@users.noreply.github.com>
2023-03-29 15:19:18 +0200

    finalized version

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import { ConnectedDashboard } from './Dashboard';
+import { ConnectedLogin } from './Login';
 import { Router, Route} from 'react-router-dom';
 import { history } from '../app/store/history';
 import { ConnectedNavigation } from './Navigation';
 import { ConnectedTaskDetail } from './TaskDetail';
+import { Redirect } from 'react-router-dom';
+
+const RouteGuard = Component => ({match}) => {
+    console.info("Route guard", match);
+    if (!store.getState().session.authenticated) {
+        return <Redirect to="/" />
+    }
+    return <Component match={match} />
+}
 
 export const Main = () => (
     <Router history={history}>
@@ -13,15 +23,16 @@ export const Main = () => (
         <div>
             <ConnectedNavigation />
             {/* <ConnectedDashboard /> */}
+            <Route exact path="/" component={ConnectedLogin} />
             <Route 
                 exact 
                 path="/dashboard" 
-                render={ ()=> (<ConnectedDashboard />)} 
+                render={ RouteGuard(ConnectedDashboard)} 
             />
             <Route 
                 exact 
                 path="/task/:id" 
-                render={ ({match})=> (<ConnectedTaskDetail match={match} />)} 
+                render={ RouteGuard(ConnectedTaskDetail)} 
             />
         </div>
     </Provider>
